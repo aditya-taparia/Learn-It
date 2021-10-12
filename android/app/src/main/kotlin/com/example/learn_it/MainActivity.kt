b7274a52387fedaebd1f12d2eb97d5d3af839675
Blue_Mist <74455854+aditya-taparia@users.noreply.github.com>
2021-10-12 19:56:04 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/learn_it/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_it
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Learn It',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          centerTitle: true,
+        ),
+        body: const Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Learn It App'),
+    );
+  }
+}
