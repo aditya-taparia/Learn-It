f01c6416863e297c5d8361a3c1377bfc7090538c
Aditya Taparia <74455854+aditya-taparia@users.noreply.github.com>
2021-10-30 20:27:08 +0530

    Add files via upload

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class Users {
+  final String userid;
+
+  Users({required this.userid});
+}
+
+class UserRole {
+  final String userid;
+  final String role;
+
+  UserRole({required this.userid, required this.role});
+}
+
+class UserData {}
