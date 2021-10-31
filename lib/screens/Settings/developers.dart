1ad68dcc59d2ee869fbe7d5d12eda604a17b71d0
Som Sagar <58594538+somsagar07@users.noreply.github.com>
2021-10-31 22:28:29 +0530

    Update developers.dart

--- lib/screens/Settings/developers.dart
+++ lib/screens/Settings/developers.dart
@@ -12,58 +12,129 @@ class _DeveloperState extends State<Developer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Our Developers"),
-          backgroundColor: Color.fromRGBO(0, 74, 140, 1),
-        ),
-        body: ListView(
-          children: [
-            CarouselSlider(
-              items: [
-                //item 1
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/sagar.png',
-                      height: 280.0,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(
-                      height: 80,
-                    ),
-                    Text("Som Sagar"),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/aditya.png',
-                      height: 280.0,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(
-                      height: 80,
-                    ),
-                    Text("Aditya Taparia"),
-                  ],
-                ),
-              ],
-              options: CarouselOptions(
-                height: 500.0,
-                enlargeCenterPage: true,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enableInfiniteScroll: true,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                viewportFraction: 0.8,
+      appBar: AppBar(
+        title: Text("Our Developers"),
+        backgroundColor: Color.fromRGBO(0, 74, 140, 1),
+      ),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            items: [
+              //item 1
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/sagar.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Som Sagar"),
+                ],
               ),
-            )
-          ],
-        ));
+              //list 2
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/aditya.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Aditya Taparia"),
+                ],
+              ),
+              //item 3
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/jeetesh.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Jeetesh Gavande"),
+                ],
+              ),
+              //list 4
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/cyril.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Cyril Kunjumon"),
+                ],
+              ),
+              //list 5
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/asui.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Ringriangsuiyang Pamei"),
+                ],
+              ),
+              //list 6
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/kowsik.png',
+                    height: 280.0,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text("Saikowsik Vellaturi"),
+                ],
+              ),
+            ],
+            options: CarouselOptions(
+              height: 500.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color.fromRGBO(0, 74, 140, 1),
+        child: const Icon(Icons.mail_rounded),
+      ),
+    );
   }
 }
