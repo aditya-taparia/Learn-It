a5a0693d34898d82c02974e90c7863ec133418d3
Aditya Taparia <74455854+aditya-taparia@users.noreply.github.com>
2021-11-02 19:41:10 +0530

    Update course.dart

--- lib/models/course.dart
+++ lib/models/course.dart
@@ -1,6 +1,12 @@
 class Course {
   final String coursename;
   final String teachername;
+  final String description;
+  final List students;
 
-  Course({required this.coursename, required this.teachername});
+  Course(
+      {required this.coursename,
+      required this.teachername,
+      required this.description,
+      required this.students});
 }
