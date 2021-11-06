17cd49fd59bc6d7ad0f301435ee25d6a39b2f269
Som Sagar <58594538+somsagar07@users.noreply.github.com>
2021-11-07 03:12:24 +0530

    Update course.dart

--- lib/models/course.dart
+++ lib/models/course.dart
@@ -11,4 +11,14 @@ class Course {
       required this.description,
       required this.students,
       required this.teacherid});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'coursename': coursename,
+      'teachername': teachername,
+      'description': description,
+      'enrolled_students': students,
+      'teacherid': teacherid,
+    };
+  }
 }
