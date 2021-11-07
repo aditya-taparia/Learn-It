db2d8b401a7449c19a2cfe2ffa9fcba4d240e2b3
Som Sagar <58594538+somsagar07@users.noreply.github.com>
2021-11-08 02:24:07 +0530

    Update student_board.dart

--- lib/screens/Home/Student/Dashboard/student_board.dart
+++ lib/screens/Home/Student/Dashboard/student_board.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:learn_it/models/course.dart';
 import 'package:learn_it/models/user.dart';
+import 'package:learn_it/services/coursedatabase.dart';
 import 'package:learn_it/services/userdatabase.dart';
 import 'package:learn_it/shared/loading.dart';
+import 'package:learn_it/shared/loading_course.dart';
 import 'package:provider/provider.dart';
 
 class StudentDashboard extends StatelessWidget {
@@ -18,6 +21,8 @@ class StudentDashboard extends StatelessWidget {
           return Scaffold(
             appBar: AppBar(
               title: const Text('Student Dashboard'),
+              backgroundColor: const Color.fromRGBO(0, 75, 141, 1),
+              centerTitle: true,
             ),
             body: Center(
               child: Column(
@@ -25,6 +30,7 @@ class StudentDashboard extends StatelessWidget {
                 children: <Widget>[
                   Text(userRole!.username),
                   Text(userRole.role),
+                  Codata(usercourse: userRole.courses)
                 ],
               ),
             ),
@@ -35,4 +41,61 @@ class StudentDashboard extends StatelessWidget {
       },
     );
   }
+}
+
+class Codata extends StatefulWidget {
+  List usercourse;
+  Codata({Key? key, required this.usercourse}) : super(key: key);
+
+  @override
+  _CodataState createState() => _CodataState();
+}
+
+class _CodataState extends State<Codata> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<List<Course>?>.value(
+        value: CourseDatabase().courses,
+        initialData: null,
+        child: Comp(usercourse: widget.usercourse));
+  }
+}
+
+class Comp extends StatefulWidget {
+  final List usercourse;
+  const Comp({Key? key, required this.usercourse}) : super(key: key);
+
+  @override
+  _CompState createState() => _CompState();
+}
+
+class _CompState extends State<Comp> {
+  @override
+  Widget build(BuildContext context) {
+    final courses = Provider.of<List<Course>?>(context);
+    return Container(child: cou(widget.usercourse, courses));
+  }
+}
+
+cou(List usercourse, List<Course>? courses) {
+  List sub = [];
+  if (courses == null) {
+    return const Center(
+      child: Text("Loading"),
+    );
+  }
+  if (courses.isEmpty) {
+    return const Center(
+      child: Text("Empty"),
+    );
+  } else {
+    for (int i = 0; i < courses.length; i++) {
+      for (int x = 0; x < usercourse.length; x++) {
+        if (courses[i].courseid == usercourse[x]) {
+          sub.add(courses[i].coursename);
+        }
+      }
+    }
+  }
+  return Text(sub.toString());
 }
